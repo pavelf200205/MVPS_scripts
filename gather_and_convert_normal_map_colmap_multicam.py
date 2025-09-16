d1c583da2b354fa725ea11cb2fd45dc4aa2e8841
pavelf200205 <pavelf200205@gmail.com>
2025-09-16 18:20:09 +0300

    Cleaned up the normal map gathering scripts

--- gather_and_convert_normal_map_colmap_multicam.py
+++ gather_and_convert_normal_map_colmap_multicam.py
@@ -1,10 +1,7 @@
 import os
-import cv2
 import pyexr
-from glob import glob
 import numpy as np
 import shutil
-from bs4 import BeautifulSoup  # $ pip install beautifulsoup4 lxml
 import argparse
 
 def make4x4(P):
@@ -101,32 +98,3 @@ for line in image_lines:
     # Save the world space normal map
     normal_map_world_file = os.path.join(normal_map_world_dir, f"{view_id}.exr")
     pyexr.write(normal_map_world_file, normal_world) 
-
-
-
-
-
-# normal_map_all = []
-# normal_map_path_all = []
-# for i in range(num_views):
-#     view_dir = os.path.join(args.sdm_unips_result_dir, f"view_{i:02d}.data")
-#     for tag in b_unique:
-#         img_name = tag.get("label")
-#         view_idx = int(img_name.split("_")[-1])
-#         # camera to world transform
-#         if view_idx == i:
-#             C2W = np.array([float(i) for i in tag.find("transform").text.split(" ")]).reshape((4, 4))
-#             R = C2W[:3, :3]
-#             break
-#     if os.path.exists(view_dir):
-#         # copy normal map
-#         normal_map_file = os.path.join(view_dir, "normal.exr")
-#         new_normal_map_file = os.path.join(normal_map_camera_dir, f"{i:02d}.exr")
-#         shutil.copy(normal_map_file, new_normal_map_file)
-
-#         # convert normal map to world space
-#         normal_map_camera = pyexr.read(new_normal_map_file)
-#         normal_map_camera[..., [1, 2]] *= -1  # revert y and z axis to match opencv conversion, X right, Y down, Z front
-#         H, W = normal_map_camera.shape[:2]
-#         normal_world = (R @ normal_map_camera.reshape(-1, 3).T).T.reshape([H, W, 3])
-#         pyexr.write(os.path.join(normal_map_world_dir, f"{i:02d}.exr"), normal_world)
